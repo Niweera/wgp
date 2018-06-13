052cb906fcf5fc6b5f52f7ff9095d38fa1d8493b
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-06-13 12:06:04 +0530

    Initial Commit
    First commit of the web group assignment,
    includes home page
    admin page and other supporting admin pages

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
-# online-technical-support-management-system
+<?php
+    include 'header.php';
+?> 
 
-How to add the header and the footer to your webpage
-
-First include header.php file
-then include headerfile.php file
-then write your body (don't type <body> tag again since it is included in header.php file
-after the content is written then include footer.php file
-then close body tag  
-then close html tag  
-Remember to save the file as a php file. (example -> file.php)
-  
- //////////example/////////
- 
- 
-    <?php
-        include 'header.php';
-    ?> 
-    <?php
-        include 'headerfile.php';
-    ?> 
-    
+    <header>
+        <div class="navbar">
+            <a href="./" class="logo">FIND LANKA</a>
+            <div class="rightnav">
+                <a class="active" href="./">Home</a>
+                <a href="#about">About Us</a>
+                <a href="#contact">Contact Us</a>
+                <a href="#services">Services</a>
+                <a href="#help">Help</a>
+                <a href="#login">Log in</a>
+            </div>
+        </div>
+    </header>
     <p> this is some sample text!</p>
     <p> this is some sample text!</p>
     <p> this is some sample text!</p>
@@ -45,5 +40,5 @@ Remember to save the file as a php file. (example -> file.php)
         include 'footer.php';
     ?> 
 
-    </body>
-    </html>
+</body>
+</html>
---
+++ users.php
@@ -0,0 +1,26 @@
+<?php
+    include 'header.php';
+?>
+
+<?php
+    include 'userheader.php';
+?>  
+
+	<h1>User authentication</h1>
+	<a href="create.php"><button type="button">Create profile</button></a><br><br>
+	<a href="delete.php"><button type="button">Delete profile</button></a><br><br>
+	<a href="edit.php"><button type="button">Edit profile</button></a>
+
+	<footer>
+    <div class="footer" style="position:fixed">
+      <p class="footerleft"> &copy; Find Lanka Incorporated. 2018 | All Rights Reserved.</p>
+      <div class="footerright">
+        <a href="#home">Link 1</a>
+        <a href="#about">Link 2</a>
+        <a href="#contact">Link 2</a>
+      </div>
+      <div style="clear: both;"></div>
+    </div>
+	</footer>
+	</body>
+</html>
