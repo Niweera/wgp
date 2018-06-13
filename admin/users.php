e21a5baf84f0a1f9802d161faddeb9e169bf18db
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2018-06-14 00:03:41 +0530

    Update users.php
    added some internal css

--- admin/users.php
+++ admin/users.php
@@ -5,12 +5,38 @@
 <?php
     include 'userheader.php';
 ?>  
+<style>
+	button {
+		background-color: #333333; 
+		border: none;
+		color: white;
+		padding: 15px 80px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 16px;
+		margin: 20px 20px 15px 20px;
+			}
+	.center {
+		margin: auto ;
+		margin-top: 15px;
+		width: 50%;
+		padding: 10px;
+		text-align: center;
+		border: 2px solid grey;
+		background-color: #c6cbd3;
+			}
+	button:hover {
+		 background-color: #111111;
+		color: white; }
+</style>
 
+      <div class ="center">
 	<h1>User authentication</h1>
 	<a href="create.php"><button type="button">Create profile</button></a><br><br>
 	<a href="delete.php"><button type="button">Delete profile</button></a><br><br>
 	<a href="edit.php"><button type="button">Edit profile</button></a>
-
+</div>
 	<footer>
     <div class="footer" style="position:fixed">
       <p class="footerleft"> &copy; Find Lanka Incorporated. 2018 | All Rights Reserved.</p>
