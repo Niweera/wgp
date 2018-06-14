994a85d5b6136ecbb69ff52cf96e078a87cb20c0
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-06-15 01:38:31 +0530

    Minor edit in delete.php
    Just removed unnecessary codes

--- admin/delete.php
+++ admin/delete.php
@@ -69,96 +69,83 @@
 			}
 		</style>
 		
-	<div class="content">
-	<div class="content-inside">
-		<!--<header>
-			<div class="navbar">
-				<a href="../" class="logo">FIND LANKA</a>
-				<div class="rightnav">
-					<a href="./">Home</a>
-					<a class="active" href="./users.php">User Administration</a>
-					<a href="#contact">Customer Requests</a>
-					<a href="#services">Technicians</a>
-					<a href="#help">Reports</a>
-					<a href="#login">Log out</a>
-				</div>
-			</div>
-		</header>-->
-		<?php
-			include 'userheader.php';
-		?>
-		<h1>Search & Delete</h1>
-		<form method="post" action="delete.php" class="center">
-			<p style="font-size:18px;display: inline">Enter username:</p> 
-			<input type="text" name="uname"><br><br>
-			<button type="submit" name="sub">Search Customer</button>
-			<button type="submit" name="sub2">Search Technician</button><br><br>
-		</form>
+		<div class="content">
+			<div class="content-inside">
+				<?php
+					include 'userheader.php';
+				?>
+				<h1>Search & Delete</h1>
+				<form method="post" action="delete.php" class="center">
+					<p style="font-size:18px;display: inline">Enter username:</p> 
+					<input type="text" name="uname"><br><br>
+					<button type="submit" name="sub">Search Customer</button>
+					<button type="submit" name="sub2">Search Technician</button><br><br>
+				</form>
 
-		<?php 
-			if (null !==(filter_input(INPUT_POST, 'sub'))){
-				$uname=mysqli_real_escape_string($conn,filter_input(INPUT_POST, 'uname'));
-				$sql = "SELECT CustID, ContactNo, Email FROM Customer WHERE CustID='$uname';";
-				$result=mysqli_query($conn,$sql);
-				$queryResult=mysqli_num_rows($result);
-				if ($queryResult > 0){
-					echo "<br/><p style=\"font-size:18px;text-align:center\">User is available</p>";
-					echo "<div class = \"rest\">";
-					echo "<table style=\"width:100%\">";
-					echo "<tr><th>Username</th><th>Contact No</th><th>Email</th></tr>";
-					while ($row=mysqli_fetch_assoc($result)){
-						$uname = $row['CustID'];
-						$cont = $row['ContactNo'];
-						$email = $row['Email'];
-						echo "<tr><td>".$uname."</td><td>".$cont."</td><td>".$email."</td></tr>";    
+				<?php 
+					if (null !==(filter_input(INPUT_POST, 'sub'))){
+						$uname=mysqli_real_escape_string($conn,filter_input(INPUT_POST, 'uname'));
+						$sql = "SELECT CustID, ContactNo, Email FROM Customer WHERE CustID='$uname';";
+						$result=mysqli_query($conn,$sql);
+						$queryResult=mysqli_num_rows($result);
+						if ($queryResult > 0){
+							echo "<br/><p style=\"font-size:18px;text-align:center\">User is available</p>";
+							echo "<div class = \"rest\">";
+							echo "<table style=\"width:100%\">";
+							echo "<tr><th>Username</th><th>Contact No</th><th>Email</th></tr>";
+							while ($row=mysqli_fetch_assoc($result)){
+								$uname = $row['CustID'];
+								$cont = $row['ContactNo'];
+								$email = $row['Email'];
+								echo "<tr><td>".$uname."</td><td>".$cont."</td><td>".$email."</td></tr>";    
+								}
+							echo "</table>";
+							echo "</div>";
+						}else {
+							echo "<p style=\"text-align:center\">User not available</p>";
 						}
-					echo "</table>";
-					echo "</div>";
-				}else {
-					echo "<p style=\"text-align:center\">User not available</p>";
-				}
-			}
-		
-			if (null !==(filter_input(INPUT_POST, 'sub2'))){
-				$uname=mysqli_real_escape_string($conn,filter_input(INPUT_POST, 'uname'));
-				$sql = "SELECT TechID, FirstName, LastName, ContactNo, Email, Occupation, City FROM Technician WHERE TechID='$uname';";
-				$result=mysqli_query($conn,$sql);
-				$queryResult=mysqli_num_rows($result);
-				if ($queryResult > 0){
-					echo "<br/><p style=\"font-size:18px;text-align:center\">User is available</p><br/><br/>";
-					echo "<div class = \"rest\">";
-					echo "<table style=\"width:50%\">";
-					echo "<tr><th>Username</th><th>First Name</th><th>Last Name</th><th>Contact No</th><th>Email</th><th>Occupation</th><th>City</th></tr>";
-					while ($row=mysqli_fetch_assoc($result)){
-						$uname = $row['TechID'];
-						$fname = $row['FirstName'];
-						$lname = $row['LastName'];
-						$cont = $row['ContactNo'];
-						$email = $row['Email'];
-						$ocp = $row['Occupation'];
-						$city = $row['City'];
-						echo "<tr><td>".$uname."</td><td>".$fname."</td><td>".$lname."</td><td>".$cont."</td><td>".$email."</td><td>".$ocp."</td><td>".$city."</td></tr>";    
 					}
-					echo "</table>";
-					echo "</div>";
-					
-				}else {
-					echo "<p style=\"text-align:center\">User not available</p>";
-				}
-			}
-		?>
+				
+					if (null !==(filter_input(INPUT_POST, 'sub2'))){
+						$uname=mysqli_real_escape_string($conn,filter_input(INPUT_POST, 'uname'));
+						$sql = "SELECT TechID, FirstName, LastName, ContactNo, Email, Occupation, City FROM Technician WHERE TechID='$uname';";
+						$result=mysqli_query($conn,$sql);
+						$queryResult=mysqli_num_rows($result);
+						if ($queryResult > 0){
+							echo "<br/><p style=\"font-size:18px;text-align:center\">User is available</p><br/><br/>";
+							echo "<div class = \"rest\">";
+							echo "<table style=\"width:50%\">";
+							echo "<tr><th>Username</th><th>First Name</th><th>Last Name</th><th>Contact No</th><th>Email</th><th>Occupation</th><th>City</th></tr>";
+							while ($row=mysqli_fetch_assoc($result)){
+								$uname = $row['TechID'];
+								$fname = $row['FirstName'];
+								$lname = $row['LastName'];
+								$cont = $row['ContactNo'];
+								$email = $row['Email'];
+								$ocp = $row['Occupation'];
+								$city = $row['City'];
+								echo "<tr><td>".$uname."</td><td>".$fname."</td><td>".$lname."</td><td>".$cont."</td><td>".$email."</td><td>".$ocp."</td><td>".$city."</td></tr>";    
+							}
+							echo "</table>";
+							echo "</div>";
+							
+						}else {
+							echo "<p style=\"text-align:center\">User not available</p>";
+						}
+					}
+				?>
 
-		<hr><br>
+				<hr><br>
 
-		<form action="delete.php" method="post" class="center">
-			<p style="font-size:18px;display: inline">Enter username to delete: </p>
-			<input type="text" name="dname"><br><br>
-			<button type="submit" name="del">Delete Customer</button>
-			<button type="submit" name="del2" onclick=myfunction()>Delete Technician</button>
-		</form>
+				<form action="delete.php" method="post" class="center">
+					<p style="font-size:18px;display: inline">Enter username to delete: </p>
+					<input type="text" name="dname"><br><br>
+					<button type="submit" name="del">Delete Customer</button>
+					<button type="submit" name="del2" onclick=myfunction()>Delete Technician</button>
+				</form>
 
-	</div>
-	</div>
+			</div>
+		</div>
 
 		<footer class="footer">
 			<p class="footerleft" style="margin-top:0"> &copy; Find Lanka Incorporated. 2018 | All Rights Reserved.</p>
@@ -169,7 +156,7 @@
 			</div>
 			<div style="clear: both"></div>
 		</footer>
-
+		
 	</body>
 </html>
 
