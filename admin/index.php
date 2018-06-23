7b899fa840a9b146aa2f25dc49a3ab0f9537d09e
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2018-06-23 23:38:17 +0530

    minor update

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@
         <div class="rightnav">
           <a class="active" href="./">Home</a>
           <a href="./users.php">User Administration</a>
-          <a href="./customer.phpp">Customers</a>
+          <a href="./customer.php">Customers</a>
           <a href="./technician.php">Technicians</a>
           <a href="#help">Reports</a>
           <a href="#login">Log out</a>
