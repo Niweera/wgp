f14a6b71cac10e0dd1534a5f74a1d9c3ea193040
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2018-06-23 23:29:19 +0530

    major update
    simplified the code

--- admin/technician.php
+++ admin/technician.php
@@ -48,77 +48,18 @@
 <!--the page views the technician database-->
 	    <!--ALSO THIS CODE CAN BE SIMPLIFIED-->
 		<h1 style="text-align:center;">Technician Database</h1>
-		<form action="newtech.php" method="post" class="center">
-			<input type="radio" name="radio" <?php if (isset($_POST['radio']) && ($_POST['radio'])=="car") {echo "checked";}?> value="car" >Carpenter
-			<input type="radio" name="radio" <?php if (isset($_POST['radio']) && ($_POST['radio'])=="elec") {echo "checked";}?> value="elec">Electrician
-			<input type="radio" name="radio" <?php if (isset($_POST['radio']) && ($_POST['radio'])=="it") {echo "checked";}?> value="it">IT Technician
+		<form action="technician.php" method="post" class="center">
+			<input type="radio" name="radio" <?php if (isset($_POST['radio']) && ($_POST['radio'])=="Carpenter") {echo "checked";}?> value="Carpenter" >Carpenter
+			<input type="radio" name="radio" <?php if (isset($_POST['radio']) && ($_POST['radio'])=="Electrician") {echo "checked";}?> value="Electrician">Electrician
+			<input type="radio" name="radio" <?php if (isset($_POST['radio']) && ($_POST['radio'])=="IT technician") {echo "checked";}?> value="IT technician">IT Technician
 			<input type="radio" name="radio" <?php if (isset($_POST['radio']) && ($_POST['radio'])=="all") {echo "checked";}?> value="all">All
 			<input type="submit" name="Search" value="Search" style="width:80px;height:30px;margin-left:150px;"><br>
 		</form>
-
-		<?php
+	<?php
 if (isset($_POST['Search'])) {
-	 $occupation=$_POST['radio']; 
-if(isset($occupation) && $occupation=="car")
-
-{
-	
-	$sql = "SELECT TechID, FirstName, LastName, Email, ContactNo FROM technician WHERE Occupation='Carpenter';";
-						$result=mysqli_query($conn,$sql);
-						$queryResult=mysqli_num_rows($result);
-						if ($queryResult > 0){
-							echo "<table>";
-							echo "<tr><th>TechID</th><th>FirstName</th><th>LastName</th><th>Email</th><th>ContactNo</th></tr>";
-							while ($row=mysqli_fetch_assoc($result)){
-								$uname = $row['TechID'];
-								$first = $row['FirstName'];
-								$last = $row['LastName'];
-								$email = $row['Email'];
-								$cont = $row['ContactNo'];
-								echo "<tr><td>".$uname."</td><td>".$first."</td><td>".$last."</td><td>".$email."</td><td>".$cont."</td></tr>";    
-								}
-						echo "</table>";}
-	}
-if(isset($occupation) && $occupation=="elec")
-{
-	$sql = "SELECT TechID, FirstName, LastName, Email, ContactNo FROM technician WHERE Occupation='Electrician';";
-						$result=mysqli_query($conn,$sql);
-						$queryResult=mysqli_num_rows($result);
-						if ($queryResult > 0){
-							echo "<table>";
-							echo "<tr><th>TechID</th><th>FirstName</th><th>LastName</th><th>Email</th><th>ContactNo</th></tr>";
-							while ($row=mysqli_fetch_assoc($result)){
-								$uname = $row['TechID'];
-								$first = $row['FirstName'];
-								$last = $row['LastName'];
-								$email = $row['Email'];
-								$cont = $row['ContactNo'];
-								echo "<tr><td>".$uname."</td><td>".$first."</td><td>".$last."</td><td>".$email."</td><td>".$cont."</td></tr>";    
-								}
-						echo "</table>";}
-	}
-if(isset($occupation) && $occupation=="it")
-{
-	$sql = "SELECT TechID, FirstName, LastName, Email, ContactNo FROM technician WHERE Occupation='IT Technician';";
-						$result=mysqli_query($conn,$sql);
-						$queryResult=mysqli_num_rows($result);
-						if ($queryResult > 0){
-							echo "<table>";
-							echo "<tr><th>TechID</th><th>FirstName</th><th>LastName</th><th>Email</th><th>ContactNo</th></tr>";
-							while ($row=mysqli_fetch_assoc($result)){
-								$uname = $row['TechID'];
-								$first = $row['FirstName'];
-								$last = $row['LastName'];
-								$email = $row['Email'];
-								$cont = $row['ContactNo'];
-								echo "<tr><td>".$uname."</td><td>".$first."</td><td>".$last."</td><td>".$email."</td><td>".$cont."</td></tr>";    
-								}
-						echo "</table>";}
-	}
-	
-if(isset($occupation) && $occupation=="all")
-{
-	$sql = "SELECT TechID, FirstName, LastName, Email, ContactNo, Occupation FROM technician;";
+	$occupation=$_POST['radio'];
+	if ($occupation == "all") {
+		$sql = "SELECT TechID, FirstName, LastName, Email, ContactNo, Occupation FROM technician;";
 						$result=mysqli_query($conn,$sql);
 						$queryResult=mysqli_num_rows($result);
 						if ($queryResult > 0){
@@ -133,12 +74,25 @@ if(isset($occupation) && $occupation=="all")
 								$occ = $row['Occupation'];
 								echo "<tr><td>".$uname."</td><td>".$first."</td><td>".$last."</td><td>".$email."</td><td>".$cont."</td><td>".$occ."</td></tr>";    
 								}
-						echo "</table>";}
-	}
-}				
-		?>
-		
-			
+							echo "</table>";}}
+	else {
+	 
+	$sql = "SELECT TechID, FirstName, LastName, Email, ContactNo FROM technician WHERE Occupation='$occupation';";
+						$result=mysqli_query($conn,$sql);
+						$queryResult=mysqli_num_rows($result);
+						if ($queryResult > 0){
+							echo "<table>";
+							echo "<tr><th>TechID</th><th>FirstName</th><th>LastName</th><th>Email</th><th>ContactNo</th></tr>";
+							while ($row=mysqli_fetch_assoc($result)){
+								$uname = $row['TechID'];
+								$first = $row['FirstName'];
+								$last = $row['LastName'];
+								$email = $row['Email'];
+								$cont = $row['ContactNo'];
+								echo "<tr><td>".$uname."</td><td>".$first."</td><td>".$last."</td><td>".$email."</td><td>".$cont."</td></tr>";    
+								}
+						echo "</table>"; }}}
+	?>
 		<footer>
 			<div class="footer" style="position:fixed">
 				<p class="footerleft"> &copy; Find Lanka Incorporated. 2018 | All Rights Reserved.</p>
