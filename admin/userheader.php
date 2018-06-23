c6669b56291c0101fb84f1e8d66fd284da623d83
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2018-06-23 21:23:23 +0530

    minor update
    added customer and technician php files

--- admin/userheader.php
+++ admin/userheader.php
@@ -4,8 +4,8 @@
         <div class="rightnav">
             <a href="./">Home</a>
             <a class="active" href="./users.php">User Administration</a>
-            <a href="#contact">Customers</a>
-            <a href="#services">Technicians</a>
+            <a href="./customer.php">Customers</a>
+            <a href="./technician.php">Technicians</a>
             <a href="#help">Reports</a>
             <a href="#login">Log out</a>
         </div>
