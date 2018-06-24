138414354a5cb173acdefd8603ee406b33f6251f
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-06-25 00:08:29 +0530

    Major updates
    added php support to the select options to add infinite no of select options

--- admin/create.php
+++ admin/create.php
@@ -16,7 +16,19 @@
 				Email: <input type="text" name="email" value="" style="margin-left:90px" required/><br><br>
 				Password: <input type="password" name="pw" value="" style="margin-left:60px" required/><br><br>
 				City: 
-				<select name="city" style="margin-left:102px">
+				<!--added php support to add infinite no of select options-->
+				<?php 
+					$result = $conn->query("select cityname from city");	
+					echo "<select name=\"city\" style=\"margin-left:102px\">";
+					echo '<option value="">Leave empty for a customer</option>';
+					while ($row = $result->fetch_assoc()) {
+						unset($id, $name);
+						$name = $row['cityname']; 
+						echo '<option value="'.$name.'">'.$name.'</option>'; 
+					}
+					echo "</select><br><br>";
+				?>
+				<!--<select name="city" style="margin-left:102px">
 						<option value="">Leave empty for a customer</option>
 						<option value="Colombo">Colombo</option>
 						<option value="Kalutara">Kalutara</option>
@@ -24,8 +36,20 @@
 						<option value="Matara">Matara</option>
 						<option value="Kandy">Kandy</option>
 						<option value="Kurunegala">Kurunegala</option>	
-					</select><br><br>
+					</select><br><br>-->
 				Occupation:
+				<?php 
+					$result = $conn->query("select typename from techtype");	
+					echo "<select name=\"ocp\" style=\"margin-left:48px\">";
+					echo '<option value="">Leave empty for a customer</option>';
+					while ($row = $result->fetch_assoc()) {
+						unset($id, $name);
+						$name = $row['typename']; 
+						echo '<option value="'.$name.'">'.$name.'</option>'; 
+					}
+					echo "</select><br><br>";
+				?>
+				<!--
 				<select name="ocp" style="margin-left:48px">
 					<option value="">Leave empty for a customer</option>
 					<option value="IT Technician">IT Technician</option>
@@ -34,8 +58,21 @@
 					<option value="Mechanic">Mechanic</option>
 					<option value="Plumber">Plumber</option>
 					<option value="Welder">Welder</option>	
-				</select><br><br>
+				</select><br><br>-->
 				Skill:
+				<?php 
+					$result = $conn->query("select SkID, SkillName from skill");	
+					echo "<select name=\"skid\" style=\"margin-left:100px\">";
+					echo '<option value="">Leave empty for a customer</option>';
+					while ($row = $result->fetch_assoc()) {
+						unset($id, $name);
+						$id = $row['SkID'];
+						$name = $row['SkillName']; 
+						echo '<option value="'.$id.'">'.$name.'</option>'; 
+					}
+					echo "</select><br><br>";
+				?>
+				<!--
 				<select name="skid" style="margin-left:100px">
 					<option value="">Leave empty for a customer</option>
 					<option value="Ittc001">PC Troubleshooting</option>
@@ -44,7 +81,7 @@
 					<option value="Plum001">Pipelining</option>
 					<option value="Mech001">Tinkering</option>
 					<option value="Carp001">Wood works</option>	
-				</select><br><br>
+				</select><br><br>-->
 				
 				<button type="submit" name="submit" style="width:210px">Create Customer Account</button>
 				<button type="submit" name="submit2" style="width:210px">Create Technician Account</button>
