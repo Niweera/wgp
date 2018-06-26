2e3e459249f0b76581f992fb0bc3bef925a48b81
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-06-26 14:51:03 +0530

    Minor updates
    Changed the contact page

--- about/contact us.php
+++ about/contact us.php
@@ -1,159 +1,93 @@
 <?php
     include 'header.php';
 ?> 
-		<header>
-			<div class="navbar">
-				<a href="../" class="logo">FIND LANKA</a>
-				<div class="rightnav">
-					<a href="../">Home</a>
-					<div class="dropdown">
-						<button class="dropbtn"><a href="./">About Us</a> 
-						</button>
-						<div class="dropdown-content">
-							<a href="company profile.php">Company Profile</a>
-							<a href="our story.php">Our Story</a>
-							<a href="leadership & management.php">Leadership & Management</a>
+
+		<div class="content">
+			<div class="content-inside">
+				<header>
+					<div class="navbar">
+						<a href="../" class="logo">FIND LANKA</a>
+						<div class="rightnav">
+							<a href="../">Home</a>
+							<div class="dropdown">
+								<button class="dropbtn"><a href="./">About Us</a> 
+								</button>
+								<div class="dropdown-content">
+									<a href="company profile.php">Company Profile</a>
+									<a href="our story.php">Our Story</a>
+									<a href="leadership & management.php">Leadership & Management</a>
+								</div>
+							</div> 
+							<div class="dropdown">
+								<button class="dropbtn"><a class="active" href="./contact us.php">Contact Us</a> 
+								</button>
+								<div class="dropdown-content">
+									<a href="contact support.php">Contact Support</a>
+								</div>
+							</div> 
+							<a href="../services">Services</a>
+							<a href="../help">Help</a>
+							<a href="../login">Log in</a>
+							<a href="../login#signup">Sign Up</a>
 						</div>
-					</div> 
-					<div class="dropdown">
-						<button class="dropbtn"><a class="active" href="./contact us.php">Contact Us</a> 
-						</button>
-						<div class="dropdown-content">
-							<a href="contact support.php">Contact Support</a>
-						</div>
-					</div> 
-					<a href="../services">Services</a>
-					<a href="../help">Help</a>
-					<a href="../login">Log in</a>
-					<a href="../login#signup">Sign Up</a>
+					</div>
+				</header>
+
+
+				<div class = "wrapper">
+					<!--edit from here-->
+					<div class = "table2">
+						<div class = "table-call">
+							<img src = "image/call.png" alt="Image">
+							<h2>Still not listed on our site?</h2>
+							<p><b>If you are interestered in listing on Find Lanka's services,<br>
+							Call us and give your info to one of our team.</b></p>
+							<h3>011 210 0000</h3>
+						</div><!--table-call-->
+						<div class = "table-support" style="width:600px">
+							<img src = "image/text.jpg" alt="Image">
+							<h2 style="width:600px">Contact customer support</h2>
+							<p><b>Sometimes you need a little help.<br> Don't worry...... we're here for you.</b></p>
+							<form action = "contact support.php">
+								<button><b>Contact Support</b></button>
+							</form>
+						</div><!--table-support-->
+					</div><!--table2-->
+					<div class = "contact">
+						<h1 style="margin-left: 270px">Contact one of our staff at any day any time @ our office</h1>
+					</div><!--contact-->
+					<div class = "table3">
+						<!--<div class = "office-image">
+							<img src = "image/office.jpg" alt = "image" title = "Sri Lanka">
+						</div>--><!--office-image-->
+						<div class = "office-text">
+							<h2>Address:</h2>
+								<p>20/3, First Street, Colombo 07, Sri Lanka.</p>
+							<h2>Phone:</h2>
+								<p>+11 210 0000<br> +11 220 0000</p>
+							<h2>Fax:</h2>
+								<p>+11 230 0000</p>
+						</div><!--office-text-->
+					</div><!--table3-->	
+				<!--end edit from here-->
+				</div><!--wrapper-->
+
+				<div style="padding:400px">
+				<br><br>
 				</div>
 			</div>
-		</header>
-			<div class = "wrapper">
-				<!--edit from here-->
-				<div class = "table2">
-					<div class = "table-call">
-						<img src = "image/call.png" alt="Image">
-						<h2>Talk to sale</h2>
-						<p><b>Interestered in TechGuru's service?</p> 
-						<p1>Just pick up the phone to chat with a number of our sale team.</b></p1>
-						<h3>011 210 0000</h3>
-						<a href = "#contact">View all global Numbers</a>
-					</div><!--table-call-->
-					<div class = "table-support">
-						<img src = "image/text.jpg" alt="Image">
-						<h2>Contact customer support</h2>
-						<p><b>Sometimes you need a little help.<br> Don't worry...... we're here for you.</b></p>
-						<form action = "contact support.php">
-							<button><b>Contact Support</b></button>
-						</form>
-					</div><!--table-support-->
-				</div><!--table2-->
-				<div class = "contact">
-					<h1>Contact with one of Our Global Offices</h1>
-				</div><!--contact-->
-				<div class = "table3">
-					<div class = "office-image">
-						<img src = "image/office.jpg" alt = "image" title = "Sri Lanka">
-					</div><!--office-image-->
-					<div class = "office-text">
-						<h2>Address:</h2>
-							<p>20/3, First Street, Colombo 07, Sri Lanka.</p>
-						<h2>Phone:</h2>
-							<p>+11 210 0000<br> +11 220 0000</p>
-						<h2>Fax:</h2>
-							<p>+11 230 0000</p>
-					</div><!--office-text-->
-				</div><!--table3-->
-				<form action = "#office">
-					<button><b>How to find this Office</b></button>
-				</form>
-				<div class = "foot">
-					<div class = "footcol">
-						<h2>China</h2>
-						<div class = "tweet">
-							<p><b>Address:</b></p>
-								<p1>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp No.20, Harbour, Bejing, China.</p1>
-							<p><b>Phone:</b></p>
-								<p1>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp+713 2101 2101</p1>
-						</div><!--tweet-->
-					</div><!--footcol-->
-					<div class = "footcol">
-						<h2>Japan</h2>
-							<div class = "tweet">
-							<p><b>Address:</b></p>
-								<p1>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp No.447, Chiyod-ku,Tokyo, Japan.</p1>
-							<p><b>Phone:</b></p>
-								<p1>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp+813 2401 3401</p1>
-						</div><!--tweet-->
-					</div><!--footercol-->
-					<div class = "footcol">
-						<h2>India</h2>
-							<div class = "tweet">
-							<p><b>Address:</b></p>
-								<p1>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp No.200, New Delhi, India.</p1>
-							<p><b>Phone:</b></p>
-								<p1>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp+513 4254 0001</p1>
-						</div><!--tweet-->
-					</div><!--footcol-->
-					<div class = "footcol">
-						<h2>Bangladesh</h2>
-							<div class = "tweet">
-							<p><b>Address:</b></p>
-								<p1>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp No.34/5, Dhakka, Bangladesh.</p1>
-							<p><b>Phone:</b></p>
-								<p1>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp+324 5630 1111</p1>
-						</div><!--tweet-->
-					</div><!--footcol-->
-				</div><!--foot-->
-				<div class = "office" id = "office">
-					<p><strong>Driving :</strong> Refer to derections provided by <a href="https://goo.gl/maps/AZh9XycfjxM2" target="_blank"> Google Maps</a>.</p><br>
-					<p><strong>Public Transportation :</strong>The cloest option for public transportations are Colombo fort station and 120/138 main bus road.</p><br>
-					<p><strong>Parking :</strong>We recommend parking in the First Street Garage, which is right next door to the office. Entrence is on Spring Street near First Street.</p>
-				</div><!--office-->
-				<div class = "texting">
-					<h1 id = "contact">Call sales using one of Our Local Numbers</h1>
-				</div><!--texting-->
-				<div class = "table4">
-					<div class = "table-txt">
-						<p><strong>Armenia</strong><br>+215 6001 6001<br></p>
-						<p><strong>India</strong><br>+513 4254 0001<br></p>
-						<p><strong>North Korea</strong><br>+456 2561 2561<br></p>
-					</div><!--table-txt-->
-					<div class = "table-txt">
-						<p><strong>Bangladesh</strong><br>+324 5630 1111<br></p>
-						<p><strong>Iran</strong><br>+751 2456 2475<br></p>
-						<p><strong>Pakistan</strong><br>+742 5810 2800<br></p>
-					</div><!--table-txt-->
-					<div class = "table-txt">
-						<p><strong>China</strong><br>+713 2101 2101<br></p>
-						<p><strong>Isrel</strong><br>+382 5656 5676<br></p>
-						<p><strong>Sri Lanka</strong><br>+11 2100 0000<br></p>
-					</div><!--table-txt-->
-					<div class = "table-txt">
-						<p><strong>Hong Kong</strong><br>+542 6223 6424<br></p>
-						<p><strong>Japan</strong><br>+813 2401 3401<br></p>
-						<p><strong>Veitnam</strong><br>+691 2604 2600<br></p>
-						<br>
-						<br>
-						<br>
-					</div><!--table-txt-->
-				</div><!--table4-->
-			<!--end edit from here-->
-			</div><!--wrapper-->
+		</div>
 			
-			<footer>
-				<div class="footer" style="position:fixed">
-				<p class="footerleft" style="padding-top:15px"> &copy; Find Lanka Incorporated. 2018 | All Rights Reserved.</p>
-				<div class="footerright">
-					<a href="#home">Link 1</a>
-					<a href="#about">Link 2</a>
-					<a href="#contact">Link 2</a>
-				</div>
-				<div style="clear: both;"></div>
-				</div>
-			</footer>
-		</body>
-	</html>
+		<footer class="stickyfooter">
+			<p class="footerleft" style="margin-top:0"> &copy; Find Lanka Incorporated. 2018 | All Rights Reserved.</p>
+			<div class="footerright">
+				<a href="#home">Link 1</a>
+				<a href="#about">Link 2</a>
+				<a href="#contact">Link 2</a>
+			</div>
+			<div style="clear: both"></div>
+		</footer>
+	</body>
+</html>
 		
 					
