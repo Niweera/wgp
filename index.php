ea9a39a6eba45cbb561c9f6a5bce791fa23f6eaa
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-07-02 14:24:31 +0530

    Minor updates
    added stickyfooter to the homepage

--- index.php
+++ index.php
@@ -5,11 +5,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<title>Find Lanka</title>
-	<link rel="stylesheet" type="text/css" href="./main.css">
-	<link rel="stylesheet" type="text/css" href="./style.css">
-	</head>
-	<body>
+		<title>Find Lanka</title>
+		<link rel="stylesheet" type="text/css" href="./main.css">
+		<link rel="stylesheet" type="text/css" href="./style.css">
 		<style>
 			.one{
 				margin-left: 20px;
@@ -38,47 +36,80 @@
 				font-size: 30px;
 				color: grey;
 			}
+			.stickyfooter {
+				background: #333333;
+				color: white;
+				line-height: 50px;
+				padding: 0 20px;
+				height: 50px;
+				margin-top: -50px;
+			}
+			.content {
+				min-height: 100%;
+			}
+			.content-inside {
+				padding: 0px;
+				padding-bottom: 50px;
+			}
 		</style>
-		<header>
-				<div class="navbar">
-					<a href="./" class="logo">FIND LANKA</a>
-					<div class="rightnav">
-						<a class="active" href="./">Home</a>
-						<div class="dropdown">
-							<button class="dropbtn"><a href="./about">About Us</a> 
-							</button>
-							<div class="dropdown-content">
-								<a href="./about/company profile.php">Company Profile</a>
-								<a href="./about/our story.php">Our Story</a>
-								<a href="./about/leadership & management.php">Leadership & Management</a>
+	</head>
+	<body>
+		<div class="content">
+			<div class="content-inside" style="text-align:center">
+				<header>
+						<div class="navbar">
+							<a href="./" class="logo">FIND LANKA</a>
+							<div class="rightnav">
+								<a class="active" href="./">Home</a>
+								<div class="dropdown">
+									<button class="dropbtn"><a href="./about">About Us</a> 
+									</button>
+									<div class="dropdown-content">
+										<a href="./about/company profile.php">Company Profile</a>
+										<a href="./about/our story.php">Our Story</a>
+										<a href="./about/leadership & management.php">Leadership & Management</a>
+									</div>
+								</div> 
+								<div class="dropdown">
+									<button class="dropbtn"><a href="./about/contact us.php">Contact Us</a> 
+									</button>
+									<div class="dropdown-content">
+										<a href="./about/contact support.php">Contact Support</a>
+									</div>
+								</div> 
+								<a href="./services">Services</a>
+								<a href="./help">Help</a>
+								<a href="./login">Log in</a>
+								<a href="./login#signup">Sign Up</a>
 							</div>
-						</div> 
-						<div class="dropdown">
-							<button class="dropbtn"><a href="./about/contact us.php">Contact Us</a> 
-							</button>
-							<div class="dropdown-content">
-								<a href="./about/contact support.php">Contact Support</a>
-							</div>
-						</div> 
-						<a href="./services">Services</a>
-						<a href="./help">Help</a>
-						<a href="./login">Log in</a>
-						<a href="./login#signup">Sign Up</a>
-					</div>
+						</div>
+				</header>
+				<!--this page will include company details of the Find Lanka-->
+				<div class="one size style">
+					<p>This is a web platform as a solution for  day to day domestic issue we all come across in our busy lives.<br><br><br> In the city of Colombo and suburban areas , finding a plumber to fix a plumbing issue in your house is as hard as finding a needle in a haystack.<br><br><br> And this scarcity extends to all types of skilled workers such as carpenters, electricians and etc.<br><br><br>People who look for services of skilled workers can not find them easily.<br><br><br>  And skilled workers who provide these services can not find work. And we are here to build up a bridge to satisfy the requirements of the both parties.<hr>
+						
+					</p>
+
+					<p class="form">Here we provide you an online technical support system to search for best service providers in need, indeed.</p>
 				</div>
-		</header>
-		<!--this page will include company details of the Find Lanka-->
-		<div class="one size style">
-		<p>This is a web platform as a solution for  day to day domestic issue we all come across in our busy lives.<br><br><br> In the city of Colombo and suburban areas , finding a plumber to fix a plumbing issue in your house is as hard as finding a needle in a haystack.<br><br><br> And this scarcity extends to all types of skilled workers such as carpenters, electricians and etc.<br><br><br>People who look for services of skilled workers can not find them easily.<br><br><br>  And skilled workers who provide these services can not find work. And we are here to build up a bridge to satisfy the requirements of the both parties.<hr>
-			
-		</p>
+				<div class="one size style">
+					<p>This is a web platform as a solution for  day to day domestic issue we all come across in our busy lives.<br><br><br> In the city of Colombo and suburban areas , finding a plumber to fix a plumbing issue in your house is as hard as finding a needle in a haystack.<br><br><br> And this scarcity extends to all types of skilled workers such as carpenters, electricians and etc.<br><br><br>People who look for services of skilled workers can not find them easily.<br><br><br>  And skilled workers who provide these services can not find work. And we are here to build up a bridge to satisfy the requirements of the both parties.<hr>
+						
+					</p>
 
-		<p class="form">Here we provide you an online technical support system to search for best service providers in need, indeed.</p>
+					<p class="form">Here we provide you an online technical support system to search for best service providers in need, indeed.</p>
+				</div>
+			</div>
 		</div>
-
-		<?php
-			include 'footer.php';
-		?> 
+		<footer class="stickyfooter">
+			<p class="footerleft" style="margin-top:0px;margin-bottom:0px"> &copy; Find Lanka Incorporated. 2018 | All Rights Reserved.</p>
+			<div class="footerright">
+				<a href="#home">Link 1</a>
+				<a href="#about">Link 2</a>
+				<a href="#contact">Link 2</a>
+			</div>
+			<div style="clear: both"></div>
+		</footer>
 
 	</body>
 </html>
