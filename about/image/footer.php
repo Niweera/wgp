d8fd5c7b3abbea5cfd28fcee1e9a7bdaffc2930c
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-07-12 23:18:46 +0530

    Removed unncessary files

--- about/image/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="stickyfooter">
-			<p class="footerleft" style="margin-top:0"> &copy; Find Lanka Incorporated. 2018 | All Rights Reserved.</p>
-			<div class="footerright">
-				<a href="#home">Link 1</a>
-				<a href="#about">Link 2</a>
-				<a href="#contact">Link 2</a>
-			</div>
-			<div style="clear: both"></div>
-</footer>
